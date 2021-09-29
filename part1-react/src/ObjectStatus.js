73f4dbad9229716d0be7385f3135e5fcfaaca28d
fausto.gomez <fausto.gomez.pelaez@gmail.com>
2021-09-29 09:05:00 -0500

    Aprendimos a usar renderizado condicional

--- part1-react/src/ObjectStatus.js
+++ part1-react/src/ObjectStatus.js
@@ -4,17 +4,23 @@ const ObjectStatus = () => {
     const [counters, setCounters] = useState({
         left: 0,
         rigth: 0,
-	    clicks: 0,
 	    message: 'Esto es un mensaje en el estado'
     });
+    
+    const WarningNoUsed = () => {
+	return <h1>No se ha iniciado el contador</h1>
+    }
+
+    const ListOfClicks = ({clicks}) => {
+	return <p>{clicks.join(', ')}</p>
+    }
 
     const [clicks, setClicks] = useState([]);
 
     const handleClickLeft = () => {
         setCounters({
 		...counters,
-            left: counters.left + 1,
-            clicks: counters.clicks + 1
+            left: counters.left + 1
         });
 	setClicks((prevClicks) => [...prevClicks, "L"]);
     };
@@ -22,8 +28,7 @@ const ObjectStatus = () => {
     const handleClickRigth = () => {
         setCounters({
 		...counters,	
-            rigth: counters.rigth + 1,
-            clicks: counters.clicks + 1
+            rigth: counters.rigth + 1
         });
 	setClicks((prevClicks) => [...prevClicks, "R"]);
     };
@@ -34,9 +39,14 @@ const ObjectStatus = () => {
         <button onClick={handleClickLeft}>Left</button>
         <button onClick={handleClickRigth}>Rigth</button>
         {counters.rigth}
-	<p>{counters.clicks}</p>
+	<p>{clicks.length}</p>
 	<p>{counters.message}</p>
-	<p>{clicks.join(", ")}</p>
+	{clicks.length === 0 ? (
+	    <WarningNoUsed />
+	) : (
+	    <ListOfClicks clicks={clicks} />
+	)
+	}
         </div>
     )
 }
