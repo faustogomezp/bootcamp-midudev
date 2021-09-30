ca86c435d2d925a910ebcd923bd7bbe5f89e0728
fausto.gomez <fausto.gomez.pelaez@gmail.com>
2021-09-30 10:41:28 -0500

    Configuramos el post para crear un post, además se independizaron los servicios o lógica del negocio de react

--- part1-react/src/App.js
+++ part1-react/src/App.js
@@ -1,56 +1,59 @@
 import "./App.css";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { Note } from "./Note.js";
-import axios from 'axios'
+import axios from "axios";
+import { getAllNotes } from './services/notes/getAllNotes.js';
+import {createNote} from './services/notes/createNote.js';
 /* import Note from '.Note.js'; Se puede usar de la misma manera con export default*/
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
+  const [newNote, setNewNote] = useState("");
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-      axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        const { data } = response;
-        setNotes(data);
-        setLoading(false);
-      } 
-    );
+    getAllNotes()
+    .then(notes =>{
+      setNotes(notes);
+      setLoading(false);
+    })
   }, []);
-  
+
   const handleChange = (event) => {
     setNewNote(event.target.value);
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const noteToAddToState = {
-      id: notes.length + 1,
-      title: newNote,
-      body: newNote
-    }
-    /* setNotes(notes.concat(noteToAddToState)); */
-    setNotes([...notes, noteToAddToState]);
-    setNewNote('');
-  }
 
+    const noteToAddToState = {
+      title: newNote,
+      body: newNote,
+      userId: 1
+    };
+
+    createNote(noteToAddToState)
+    .then(newNote => {
+      setNotes(notes.concat(newNote))
+    });
+    /* setNotes(notes.concat(noteToAddToState)); */
+    /* setNotes([...notes, noteToAddToState]); */
+    setNewNote("");
+  };
 
   return (
     <div>
       <h1>Notas</h1>
-      <p>{loading ? 'Cargando...' : '' }</p>
+      <p>{loading ? "Cargando..." : ""}</p>
       <ol>
-        {notes
-          .map((note) => (
+        {notes.map((note) => (
           <Note key={note.id} {...note} />
         ))}
       </ol>
       <form onSubmit={handleSubmit}>
-      <input type='text' onChange={handleChange} value={newNote} />
-      <button>Crear nota</button>
+        <input type="text" onChange={handleChange} value={newNote} />
+        <button>Crear nota</button>
       </form>
     </div>
   );
