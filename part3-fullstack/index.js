e214f2a3f6f750606d652a26b1578e105f4b9e9c
fausto.gomez <fausto.gomez.pelaez@gmail.com>
2021-10-05 13:07:27 -0500

    Generamos las operaciones GET, POST, DELETE de la API

--- part3-fullstack/index.js
+++ part3-fullstack/index.js
@@ -1,4 +1,4 @@
-const notes = [
+let notes = [
     {
         'id': 1,
         "content": "Me tengo que subscribir a midudev en Youtube",
@@ -19,13 +19,67 @@ const notes = [
     }
 ]
 
-const http = require('http')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+/* const http = require('http')
 
 const app = http.createServer((request, response) => {
     response.writeHead(200, {'Content-Type': 'application/json'})
     response.end(JSON.stringify(jsonFile))
+}) */
+
+
+app.get('/', (request, response) => {
+    response.send("<h1>Bienvenido a la API de bootcamp</h1>")
+})
+
+app.get('/api/notes', (request, response) => {
+    response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.filter(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+
+app.post('/api/notes', (request, response) => {
+    const note = request.body
+
+    if (!note || !note.content) {
+        return response.status(400).json({
+            error: 'note.content is missing'
+        })
+    }
+
+    const ids = notes.map(note => note.id)
+    const maxId = Math.max(...ids)
+    const newNote = {
+        "id": maxId + 1,
+        "content": note.content,
+        "important": typeof note.important !== 'undefined' ? note.important : false,
+        "date": new Date().toISOString()
+    }
+    notes = [...notes, newNote]
+    response.status(201).json(newNote)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
